2a771049c36c11e2e47c81ab84f1497d8c1b108c
Mustafa Keser <keserzade@gmail.com>
2023-01-01 02:06:58 +0300

    v1.1

---
+++ src/main/java/day15arraylists/SuleymanHocaInterview.java
@@ -0,0 +1,45 @@
+package day15arraylists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SuleymanHocaInterview {
+    /*
+    int arr[] = {3, 5, 1, 2, 7, 9, 2, 3, 5, 7};
+    By using array create a method which returns the following list
+                [3, 5+1, 2+7+9, 2+3+5+7] ==> [3, 6, 18, 17]
+    */
+
+    public static void main(String[] args) {
+        int arr[] = {3, 5, 1, 2, 7, 9, 2, 3, 5, 7};
+        addArrEl(arr);
+    }
+
+    public static void addArrEl(int arr[]){
+
+        List<Integer> list = new ArrayList<Integer>();
+        int n = 0;
+
+        while(n<arr.length) {
+            int startingIdx = n * (n + 1) / 3;
+            int endingIdx = (n + 1) * (n + 2) / 3;
+
+            int sum = 0;
+
+            if(endingIdx> arr.length){
+                break;
+            }
+            while(startingIdx<endingIdx){
+                sum = sum + arr[startingIdx];
+                startingIdx++;
+            }
+            list.add(sum);
+            n++;
+        }
+
+        System.out.println(list);
+    }
+
+
+
+}
